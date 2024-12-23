3cf826a3cf27420e9eb94a5205b92d423a8076c7
Sadiq53 <jafarussadiq098@gmail.com>
2024-12-23 19:42:41 +0530

    correction

--- frontend/src/utils/API_URL.js
+++ frontend/src/utils/API_URL.js
@@ -1,4 +1,4 @@
-const API_URL = `http://localhost:8080`
-// const API_URL = `https://indy-wash.onrender.com`
+// const API_URL = `http://localhost:8080`
+const API_URL = `https://indy-wash.onrender.com`
 
 export { API_URL }
